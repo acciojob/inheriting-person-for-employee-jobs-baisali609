58785168bd0e422a4db3611ecbe799a2d15b7dc4
baisali609 <baisali609@gmail.com>
2025-02-11 10:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   greet() {
+//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+//   }
+// }
+
+// class Employee extends Person {
+//   constructor(name, age, jobTitle) {
+//     super(name, age);
+//     this.jobTitle = jobTitle;
+//   }
+
+//   jobGreet() {
+//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+//   }
+// }
+
+
+function Person(name,age){
+	this.name = name;
+	this.age = age;
+
+}
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
   }
 
-  greet() {
-    console.log(Hello, my name is ${this.name}, I am ${this.age} years old.);
-  }
+function Employee extends Person{
+	Person.call(this, name, age);
+	this.jobTitle = jobTitle;
 }
 
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
   }
 
-  jobGreet() {
-    console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.);
-  }
-}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
-const person = new Person("Alice", 25);
-person.greet();
+// const person = new Person("Alice", 25);
+// person.greet();
 
-const employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet();
+// const employee = new Employee("Bob", 30, "Manager");
+// employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;

7b1c19b2b89326191ab075c91e7d36233170f534
baisali609 <baisali609@gmail.com>
2025-02-11 11:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-// class Person {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-//   greet() {
-//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-//   }
-// }
-
-// class Employee extends Person {
-//   constructor(name, age, jobTitle) {
-//     super(name, age);
-//     this.jobTitle = jobTitle;
-//   }
-
-//   jobGreet() {
-//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-//   }
-// }
-
-
-function Person(name,age){
-	this.name = name;
-	this.age = age;
-
-}
-Person.prototype.greet = function() {
+  greet() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
   }
-
-function Employee extends Person{
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
 }
 
-Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
   }
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
+
+
+// function Person(name,age){
+// 	this.name = name;
+// 	this.age = age;
+
+// }
+// Person.prototype.greet = function() {
+//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+//   }
+
+// function Employee extends Person{
+// 	Person.call(this, name, age);
+// 	this.jobTitle = jobTitle;
+// }
+
+// Employee.prototype.jobGreet = function() {
+//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+//   }
+
+// Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype.constructor = Employee;
 
 // const person = new Person("Alice", 25);
 // person.greet();
